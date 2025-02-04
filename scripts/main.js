e96630cfc45c16ee22b35f702e484f2dc861b9b2
Michelle-Watson <alicerockshard@gmail.com>
2025-02-04 13:52:16 -0500

    import the deafult export from axois.js using ES6 module import syntac

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+// Using the Axios ES6 Module
+
+// Q2. In your main.js file, import the default export from /libs/axios.js and give it the name axios using the ES6 module import syntax. ~ Slide 23
+// Q3. Don’t forget: when importing local modules, you must put a ./ in front of the package name in your import statement and .js at the end of the filename
+import axios from "./libs/axios.js";
