d39cf6297dc4be0eb3bb4bc336808504988515fd
Michelle-Watson <alicerockshard@gmail.com>
2025-02-04 13:54:15 -0500

    Query for the DOM element with class=dog-image and save it to a variable

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,6 @@
 // Q2. In your main.js file, import the default export from /libs/axios.js and give it the name axios using the ES6 module import syntax. ~ Slide 23
 // Q3. Don’t forget: when importing local modules, you must put a ./ in front of the package name in your import statement and .js at the end of the filename
 import axios from "./libs/axios.js";
+
+// Q4. Query for the DOM element with class="dog-image" and save it to a variable
+const dogImage = document.querySelector(".dog-image");
