f209c7a956fc5cc5208e10861055401826f66634
Michelle-Watson <alicerockshard@gmail.com>
2025-02-04 14:10:15 -0500

    make a get request using axios to random dog api, set src img to the res

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,12 @@ import axios from "./libs/axios.js";
 
 // Q4. Query for the DOM element with class="dog-image" and save it to a variable
 const dogImage = document.querySelector(".dog-image");
+
+// Q5. Make a get request to https://random.dog/woof.json?filter=mp4,webm using the default axios export you imported earlier
+// Slide 16 Ex) axios.get('http://domain.com/api')
+// Q6. After fetching the data from the Random Dog API, set the src attribute of dog image element to the value of the url property in the API response
+//
+axios.get("https://random.dog/woof.json?filter=mp4,webm").then((response) => {
+  const randomDog = response.data.url;
+  dogImage.src = randomDog;
+});
