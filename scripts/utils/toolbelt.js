998142ce5ea6c015e406b7b140d059e465857e24
Michelle-Watson <alicerockshard@gmail.com>
2025-02-04 14:25:40 -0500

    Create functions in toolbelt to export

--- scripts/utils/toolbelt.js
+++ scripts/utils/toolbelt.js
@@ -5,3 +5,19 @@
 
 const favTools = ["hammer 🔨", "screwdriver 🪛", "wrench 🔧"];
 export default favTools;
+
+// PART 2b: Named Exports and Imports
+// Q1. Create a named function called construction(). All this function should do is console.log the string 'CLANG!'
+// slide 27
+// Q2. Export this function as a named export
+export function construction() {
+  console.log("CLANG!");
+}
+
+// Q3. Create an object called 'business' to describe the properties of your construction business. It should have key-value pairs for your business name, address, and phone number
+// Q4. Export this 'business' object as a named export
+export let business = {
+  name: "ABC Company",
+  address: "Toronto, ON",
+  phone: "416-123-1234",
+};
